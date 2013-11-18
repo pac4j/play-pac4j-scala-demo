e6f92b95d851d6a3abfbc280fb914bd2eb36d005
LELEU Jerome <leleuj@gmail.com>
2013-11-18 13:12:44 +0100

    upgrade pac4j version

--- project/Build.scala
+++ project/Build.scala
@@ -12,10 +12,10 @@ object ApplicationBuild extends Build {
 //      "org.pac4j" % "play-pac4j_scala2.9" % "1.1.2-SNAPSHOT",
 //      for play 2.1 :
       "org.pac4j" % "play-pac4j_scala2.10" % "1.1.2-SNAPSHOT",
-      "org.pac4j" % "pac4j-http" % "1.4.2-SNAPSHOT",
-      "org.pac4j" % "pac4j-cas" % "1.4.2-SNAPSHOT",
-      "org.pac4j" % "pac4j-openid" % "1.4.2-SNAPSHOT",
-      "org.pac4j" % "pac4j-oauth" % "1.4.2-SNAPSHOT"
+      "org.pac4j" % "pac4j-http" % "1.5.0-SNAPSHOT",
+      "org.pac4j" % "pac4j-cas" % "1.5.0-SNAPSHOT",
+      "org.pac4j" % "pac4j-openid" % "1.5.0-SNAPSHOT",
+      "org.pac4j" % "pac4j-oauth" % "1.5.0-SNAPSHOT"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
