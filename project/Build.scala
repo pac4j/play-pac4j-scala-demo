0c01eef72235d0d95cf52f49e6e688a9acbe7ff2
Jérôme LELEU <leleuj@gmail.com>
2015-04-21 11:24:57 +0200

    Update to pac4j v1.7

--- project/Build.scala
+++ project/Build.scala
@@ -9,12 +9,13 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
 //      for play 2.0 :
-      "org.pac4j" % "play-pac4j_scala2.9" % "1.1.4",
-      "org.pac4j" % "pac4j-http" % "1.6.0",
-      "org.pac4j" % "pac4j-cas" % "1.6.0",
-      "org.pac4j" % "pac4j-openid" % "1.6.0",
-      "org.pac4j" % "pac4j-oauth" % "1.6.0",
-      "org.pac4j" % "pac4j-saml" % "1.6.0"
+      "org.pac4j" % "play-pac4j_scala2.9" % "1.1.5-SNAPSHOT",
+      "org.pac4j" % "pac4j-http" % "1.7.0",
+      "org.pac4j" % "pac4j-cas" % "1.7.0",
+      "org.pac4j" % "pac4j-openid" % "1.7.0",
+      "org.pac4j" % "pac4j-oauth" % "1.7.0",
+      "org.pac4j" % "pac4j-saml" % "1.7.0",
+      "org.pac4j" % "pac4j-oidc" % "1.7.0"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
