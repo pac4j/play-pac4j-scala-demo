07e02f042fb8ef1579277f48cdfaea589e120f36
LELEU Jérôme <leleuj@gmail.com>
2015-04-22 15:18:34 +0200

    Update Build.scala

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      "org.pac4j" % "play-pac4j_scala2.10" % "1.1.5-SNAPSHOT",
+      "org.pac4j" % "play-pac4j_scala2.10" % "1.1.5",
       "org.pac4j" % "pac4j-http" % "1.7.0",
       "org.pac4j" % "pac4j-cas" % "1.7.0",
       "org.pac4j" % "pac4j-openid" % "1.7.0",
