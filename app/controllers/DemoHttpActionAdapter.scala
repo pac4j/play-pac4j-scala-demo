ecad8cc19d0f17674225aa1bf0aa2e0575c2f2a0
Jérôme LELEU <leleuj@gmail.com>
2015-12-17 19:25:56 +0100

    Upgrade to pac4j v1.8.2

--- app/controllers/DemoHttpActionAdapter.scala
+++ app/controllers/DemoHttpActionAdapter.scala
@@ -1,19 +1,19 @@
 package controllers
 
 import org.pac4j.core.context.HttpConstants
-import org.pac4j.play.PlayWebContext
+import org.pac4j.core.context.WebContext
 import org.pac4j.play.http.DefaultHttpActionAdapter
-import play.mvc.{Results, Result}
+import play.mvc.Results
 
 class DemoHttpActionAdapter extends DefaultHttpActionAdapter {
 
-  override def handle(code: Int, context: PlayWebContext): Result = {
+  override def adapt(code: Int, context: WebContext): Object = {
     if (code == HttpConstants.UNAUTHORIZED) {
       Results.unauthorized(views.html.error401.render().toString()).as(HttpConstants.HTML_CONTENT_TYPE)
     } else if (code == HttpConstants.FORBIDDEN) {
       Results.forbidden(views.html.error403.render().toString()).as(HttpConstants.HTML_CONTENT_TYPE)
     } else {
-      super.handle(code, context)
+      super.adapt(code, context)
     }
   }
 }
