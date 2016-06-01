9741c5d3bef02df2950faa1c06a0fb9c6d4956e9
leleuj <leleuj@gmail.com>
2016-06-01 13:58:31 +0200

    update to latest lib + fix openid connect support

--- app/controllers/DemoHttpActionAdapter.scala
+++ app/controllers/DemoHttpActionAdapter.scala
@@ -1,13 +1,14 @@
 package controllers
 
 import org.pac4j.core.context.HttpConstants
-import org.pac4j.core.context.WebContext
+import org.pac4j.play.PlayWebContext
 import org.pac4j.play.http.DefaultHttpActionAdapter
 import play.mvc.Results
+import play.mvc.Result
 
 class DemoHttpActionAdapter extends DefaultHttpActionAdapter {
 
-  override def adapt(code: Int, context: WebContext): Object = {
+  override def adapt(code: Int, context: PlayWebContext): Result = {
     if (code == HttpConstants.UNAUTHORIZED) {
       Results.unauthorized(views.html.error401.render().toString()).as(HttpConstants.HTML_CONTENT_TYPE)
     } else if (code == HttpConstants.FORBIDDEN) {
