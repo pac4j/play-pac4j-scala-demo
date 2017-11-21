fbf48ab886efed92288bfb3867df857bb3b8ae2c
leleuj <leleuj@gmail.com>
2017-11-21 20:57:31 +0100

    use @Provides

--- app/modules/SecurityModule.scala
+++ app/modules/SecurityModule.scala
@@ -1,6 +1,6 @@
 package modules
 
-import com.google.inject.AbstractModule
+import com.google.inject.{AbstractModule, Provides}
 import controllers.{CustomAuthorizer, DemoHttpActionAdapter, RoleAdminAuthGenerator}
 import org.pac4j.cas.client.{CasClient, CasProxyReceptor}
 import org.pac4j.core.client.Clients
@@ -27,65 +27,13 @@ import org.pac4j.oidc.profile.OidcProfile
 import org.pac4j.saml.client.SAML2Client
 
 /**
- * Guice DI module to be included in application.conf
- */
+  * Guice DI module to be included in application.conf
+  */
 class SecurityModule(environment: Environment, configuration: Configuration) extends AbstractModule {
 
+  val baseUrl = configuration.getString("baseUrl").get
+
   override def configure(): Unit = {
-    val fbId = configuration.get[String]("fbId")
-    val fbSecret = configuration.get[String]("fbSecret")
-    val baseUrl = configuration.get[String]("baseUrl")
-
-    // OAuth
-    val facebookClient = new FacebookClient(fbId, fbSecret)
-    val twitterClient = new TwitterClient("HVSQGAw2XmiwcKOTvZFbQ", "FSiO9G9VRR4KCuksky0kgGuo8gAVndYymr4Nl7qc8AA")
-    // HTTP
-    val formClient = new FormClient(baseUrl + "/loginForm", new SimpleTestUsernamePasswordAuthenticator())
-    val indirectBasicAuthClient = new IndirectBasicAuthClient(new SimpleTestUsernamePasswordAuthenticator())
-
-    // CAS
-    val casConfiguration = new CasConfiguration("http://localhost:8888/cas/login") // ("https://casserverpac4j.herokuapp.com/login")
-    val casProxyReceptor = new CasProxyReceptor()
-    casConfiguration.setProtocol(CasProtocol.CAS20)
-    casConfiguration.setProxyReceptor(casProxyReceptor)
-    val casClient = new CasClient(casConfiguration)
-
-    // SAML
-    val cfg = new SAML2ClientConfiguration("resource:samlKeystore.jks", "pac4j-demo-passwd", "pac4j-demo-passwd", "resource:openidp-feide.xml")
-    cfg.setMaximumAuthenticationLifetime(3600)
-    cfg.setServiceProviderEntityId("urn:mace:saml:pac4j.org")
-    cfg.setServiceProviderMetadataPath(new File("target", "sp-metadata.xml").getAbsolutePath)
-    val saml2Client = new SAML2Client(cfg)
-
-    // OpenID Connect
-    val oidcConfiguration = new OidcConfiguration()
-    oidcConfiguration.setClientId("343992089165-i1es0qvej18asl33mvlbeq750i3ko32k.apps.googleusercontent.com")
-    oidcConfiguration.setSecret("unXK_RSCbCXLTic2JACTiAo9")
-    oidcConfiguration.setDiscoveryURI("https://accounts.google.com/.well-known/openid-configuration")
-    oidcConfiguration.addCustomParam("prompt", "consent")
-    val oidcClient = new OidcClient[OidcProfile](oidcConfiguration)
-    oidcClient.addAuthorizationGenerator(new RoleAdminAuthGenerator)
-
-    // REST authent with JWT for a token passed in the url as the token parameter
-    val jwtAuthenticator = new JwtAuthenticator()
-    jwtAuthenticator.addSignatureConfiguration(new SecretSignatureConfiguration("12345678901234567890123456789012"))
-    val parameterClient = new ParameterClient("token", jwtAuthenticator)
-    parameterClient.setSupportGetRequest(true)
-    parameterClient.setSupportPostRequest(false)
-
-    // basic auth
-    val directBasicAuthClient = new DirectBasicAuthClient(new SimpleTestUsernamePasswordAuthenticator)
-
-    val clients = new Clients(baseUrl + "/callback", facebookClient, twitterClient, formClient,
-      indirectBasicAuthClient, casClient, saml2Client, oidcClient, parameterClient, directBasicAuthClient,
-      new AnonymousClient(), casProxyReceptor)
-
-    val config = new Config(clients)
-    config.addAuthorizer("admin", new RequireAnyRoleAuthorizer[Nothing]("ROLE_ADMIN"))
-    config.addAuthorizer("custom", new CustomAuthorizer)
-    config.addMatcher("excludedPath", new PathMatcher().excludeRegex("^/filter/facebook/notprotected\\.html$"))
-    config.setHttpActionAdapter(new DemoHttpActionAdapter())
-    bind(classOf[Config]).toInstance(config)
 
     bind(classOf[PlaySessionStore]).to(classOf[PlayCacheSessionStore])
 
@@ -99,5 +47,82 @@ class SecurityModule(environment: Environment, configuration: Configuration) ext
     val logoutController = new LogoutController()
     logoutController.setDefaultUrl("/")
     bind(classOf[LogoutController]).toInstance(logoutController)
+  }
+
+  @Provides
+  def provideFacebookClient: FacebookClient = {
+    val fbId = configuration.getOptional[String]("fbId").get
+    val fbSecret = configuration.getOptional[String]("fbSecret").get
+    new FacebookClient(fbId, fbSecret)
+  }
+
+  @Provides
+  def provideTwitterClient: TwitterClient = new TwitterClient("HVSQGAw2XmiwcKOTvZFbQ", "FSiO9G9VRR4KCuksky0kgGuo8gAVndYymr4Nl7qc8AA")
+
+  @Provides
+  def provideFormClient: FormClient = new FormClient(baseUrl + "/loginForm", new SimpleTestUsernamePasswordAuthenticator())
+
+  @Provides
+  def provideIndirectBasicAuthClient: IndirectBasicAuthClient = new IndirectBasicAuthClient(new SimpleTestUsernamePasswordAuthenticator())
+
+  @Provides
+  def provideCasProxyReceptor: CasProxyReceptor = new CasProxyReceptor()
+
+  @Provides
+  def provideCasClient(casProxyReceptor: CasProxyReceptor) = {
+    val casConfiguration = new CasConfiguration("http://localhost:8888/cas/login") // ("https://casserverpac4j.herokuapp.com/login")
+    casConfiguration.setProtocol(CasProtocol.CAS20)
+    casConfiguration.setProxyReceptor(casProxyReceptor)
+    new CasClient(casConfiguration)
+  }
+
+  @Provides
+  def provideSaml2Client: SAML2Client = {
+    val cfg = new SAML2ClientConfiguration("resource:samlKeystore.jks", "pac4j-demo-passwd", "pac4j-demo-passwd", "resource:openidp-feide.xml")
+    cfg.setMaximumAuthenticationLifetime(3600)
+    cfg.setServiceProviderEntityId("urn:mace:saml:pac4j.org")
+    cfg.setServiceProviderMetadataPath(new File("target", "sp-metadata.xml").getAbsolutePath)
+    new SAML2Client(cfg)
+  }
+
+  @Provides
+  def provideOidcClient: OidcClient[OidcProfile] = {
+    val oidcConfiguration = new OidcConfiguration()
+    oidcConfiguration.setClientId("343992089165-i1es0qvej18asl33mvlbeq750i3ko32k.apps.googleusercontent.com")
+    oidcConfiguration.setSecret("unXK_RSCbCXLTic2JACTiAo9")
+    oidcConfiguration.setDiscoveryURI("https://accounts.google.com/.well-known/openid-configuration")
+    oidcConfiguration.addCustomParam("prompt", "consent")
+    val oidcClient = new OidcClient[OidcProfile](oidcConfiguration)
+    oidcClient.addAuthorizationGenerator(new RoleAdminAuthGenerator)
+    oidcClient
+  }
+
+  @Provides
+  def provideParameterClient: ParameterClient = {
+    val jwtAuthenticator = new JwtAuthenticator()
+    jwtAuthenticator.addSignatureConfiguration(new SecretSignatureConfiguration("12345678901234567890123456789012"))
+    val parameterClient = new ParameterClient("token", jwtAuthenticator)
+    parameterClient.setSupportGetRequest(true)
+    parameterClient.setSupportPostRequest(false)
+    parameterClient
+  }
+
+  @Provides
+  def provideDirectBasicAuthClient: DirectBasicAuthClient = new DirectBasicAuthClient(new SimpleTestUsernamePasswordAuthenticator)
+
+  @Provides
+  def provideConfig(facebookClient: FacebookClient, twitterClient: TwitterClient, formClient: FormClient, indirectBasicAuthClient: IndirectBasicAuthClient,
+                    casClient: CasClient, saml2Client: SAML2Client, oidcClient: OidcClient[OidcProfile], parameterClient: ParameterClient, directBasicAuthClient: DirectBasicAuthClient,
+                    casProxyReceptor: CasProxyReceptor): Config = {
+    val clients = new Clients(baseUrl + "/callback", facebookClient, twitterClient, formClient,
+      indirectBasicAuthClient, casClient, saml2Client, oidcClient, parameterClient, directBasicAuthClient,
+      new AnonymousClient(), casProxyReceptor)
+
+    val config = new Config(clients)
+    config.addAuthorizer("admin", new RequireAnyRoleAuthorizer[Nothing]("ROLE_ADMIN"))
+    config.addAuthorizer("custom", new CustomAuthorizer)
+    config.addMatcher("excludedPath", new PathMatcher().excludeRegex("^/filter/facebook/notprotected\\.html$"))
+    config.setHttpActionAdapter(new DemoHttpActionAdapter())
+    config
   }
 }
